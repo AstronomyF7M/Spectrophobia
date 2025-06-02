9607bc3069666941d6434f3344aa0a2c1bae63a2
AstroV3 <8769937516@student.cms.k12.nc.us>
2025-06-02 09:15:39 -0400

    Create spooky_text_generator.py

---
+++ spooky_text_generator.py
@@ -0,0 +1,19 @@
+import random
+
+phrases = [
+    "You hear footsteps behind you, but when you turn, no one's there.",
+    "A cold breath whispers your name in the darkness.",
+    "The reflection in the mirror grins, but you are not smiling.",
+    "Something is scratching at the window. It knows your name.",
+    "You find a note on your pillow: 'Don't fall asleep.'",
+    "A shadow moves just out of sight.",
+    "You wake up, but your body doesn't.",
+    "You hear someone calling from the basement. You're alone at home.",
+    "Your phone rings. The caller ID says: 'Yourself.'",
+    "The lights flicker. In the brief darkness, you see dozens of eyes."
+]
+
+print("Welcome to Spectrophobia")
+input("Press Enter")
+
+print(random.choice(phrases))
